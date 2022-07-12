6eac3fadfd1788c458423ffd8481485688814788
Willian Riviera <willx@unochapeco.edu.br>
2022-07-12 16:29:43 -0300

    Desafio 2 completo

--- src/index.js
+++ src/index.js
@@ -10,19 +10,87 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(
+    (customer) => customer.username === username
+  )
+
+  if(!user){
+    return response.status(404).json(
+      { error: "Mensagem do erro" }
+    )
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro && user.todos.length >= 10){
+    return response.status(403).json({
+      error: "Should not be able to let user create a new todo when is not Pro and already have ten todos"
+    })
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(
+    (customer) => customer.username === username
+  )
+
+  if(!user){
+    return response.status(404).json(
+      { error: "Mensagem do erro" }
+    )
+  }
+
+  if(!validate(id)){
+    return response.status(400).json(
+      { error: ""}
+    )
+  }
+
+  findTodo = user.todos.find(
+    (todo) => todo.id === id
+  )
+
+  if(!findTodo){
+    return response.status(404).json(
+      { error: "Mensagem de Erro" }
+    )
+  }
+
+  request.user = user;
+  request.todo = findTodo;
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const findUser = users.find(
+    (user) => user.id === id
+  )
+
+  if(!findUser){
+    return response.status(404).json(
+      { error: "Should not be able to pass user to request.user when it does not exists" }
+    )
+  }
+
+  request.user = findUser;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
